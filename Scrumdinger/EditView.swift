0d7034062cbe1f921ebd5fd16aeec64c10352d91
Adrian Hartanto <awi_dan@yahoo.com>
2021-06-30 01:59:03 +0700

    Finish passing data with bindings

--- Scrumdinger/EditView.swift
+++ Scrumdinger/EditView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct EditView: View {
-  @State private var scrumData: DailyScrum.Data = DailyScrum.Data()
+  @Binding var scrumData: DailyScrum.Data
   @State private var newAttendee = ""
 
   var body: some View {
@@ -48,7 +48,7 @@ struct EditView: View {
 
 struct EditView_Previews: PreviewProvider {
   static var previews: some View {
-    EditView()
+    EditView(scrumData: .constant(DailyScrum.data[0].data))
   }
 }
 
