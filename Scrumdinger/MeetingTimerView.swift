16df1766ccb3b433826cf8d659ef33aced089ff2
Adrian Hartanto <awi_dan@yahoo.com>
2021-07-01 00:01:13 +0700

    finish transcribing speech to text

--- Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/MeetingTimerView.swift
@@ -29,6 +29,7 @@ struct SpeakerArc: Shape {
 
 struct MeetingTimerView: View {
   let speakers: [ScrumTimer.Speaker]
+  let isRecording: Bool
   var scrumColor: Color
   private var currentSpeaker: String {speakers.first(where: {!$0.isCompleted})?.name ?? "Someone" }
   var body: some View {
@@ -39,15 +40,19 @@ struct MeetingTimerView: View {
         Text(currentSpeaker)
           .font(.title)
         Text("is speaking")
+        Image(systemName: isRecording ? "mic" : "mic.slash")
+          .font(.title)
+          .padding(.top)
+          .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
       }
       .accessibilityElement(children: .combine)
       .foregroundColor(scrumColor.accessibleFontColor)
       ForEach(speakers) { speaker in
         if speaker.isCompleted,
            let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
-            SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
-              .rotation(Angle(degrees: -90))
-              .stroke(scrumColor, lineWidth: 12)
+          SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+            .rotation(Angle(degrees: -90))
+            .stroke(scrumColor, lineWidth: 12)
         }
       }
     }
@@ -58,6 +63,6 @@ struct MeetingTimerView: View {
 struct MeetingTimerView_Previews: PreviewProvider {
   static var speakers = [ScrumTimer.Speaker(name: "Kim", isCompleted: true), ScrumTimer.Speaker(name: "Bill", isCompleted: false)]
   static var previews: some View {
-    MeetingTimerView(speakers: speakers, scrumColor: Color("Design"))
+    MeetingTimerView(speakers: speakers, isRecording:true, scrumColor: Color("Design"))
   }
 }
--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -1,19 +1,17 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-*/
-
 import Foundation
 
 struct History: Identifiable, Codable {
-    let id: UUID
-    let date: Date
-    var attendees: [String]
-    var lengthInMinutes: Int
+  let id: UUID
+  let date: Date
+  var attendees: [String]
+  var lengthInMinutes: Int
+  var transcript: String?
 
-    init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int) {
-        self.id = id
-        self.date = date
-        self.attendees = attendees
-        self.lengthInMinutes = lengthInMinutes
-    }
+  init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int, transcript: String? = nil) {
+    self.id = id
+    self.date = date
+    self.attendees = attendees
+    self.lengthInMinutes = lengthInMinutes
+    self.transcript = transcript
+  }
 }
